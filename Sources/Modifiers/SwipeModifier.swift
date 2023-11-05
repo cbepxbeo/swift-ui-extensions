81050013859119a874142ffad46075b906149710
cbepxbeo <cbepxbeo@mail.ru>
2023-11-05 17:13:46 +0300

    Update SwipeModifier.swift

--- Sources/Modifiers/SwipeModifier.swift
+++ Sources/Modifiers/SwipeModifier.swift
@@ -9,3 +9,46 @@
 
 import SwiftUI
 
+public struct SwipeModifier: ViewModifier {
+    
+    public enum Direction {
+        case none, left, right, up, down
+    }
+    
+    
+    public init(drag: Binding<Direction>, distance: CGFloat = 50){
+        self._drag = drag
+        self.distance = distance < 30 ? 30 : distance
+    }
+    
+    @Binding var drag: Direction
+    private let distance: CGFloat
+    private var minimum: CGFloat { distance - 1 }
+    
+    private func gesture(value: DragGesture.Value){
+        if value.translation.height > self.distance {
+            self.drag = .down
+        }
+        if value.translation.height < -self.distance {
+            self.drag = .up
+        }
+        if value.translation.width < -self.distance {
+            self.drag = .left
+        }
+        if value.translation.width > self.distance {
+            self.drag = .right
+        }
+    }
+    
+    public func body(content: Content) -> some View {
+        content
+            .gesture(DragGesture(minimumDistance: 30, coordinateSpace: .local)
+                .onChanged{
+                    self.gesture(value: $0)
+                }
+                .onEnded{ _ in
+                    drag = .none
+                }
+            )
+    }
+}
